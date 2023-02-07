4865138efe5bb8f45fc7bca559c53d3ac53c733b
Dante Gutierrez <dgutierrez@student.neumont.edu>
2023-02-07 10:32:36 -0700

    compose + response

---
+++ backend/models/response.py
@@ -0,0 +1,10 @@
+import json
+
+def response(code, body):
+    return {
+        "statusCode": code,
+        "headers": {
+            "Content-Type": "application/json"
+            },
+        "body": json.dumps(body)
+    }
