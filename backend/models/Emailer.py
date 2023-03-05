8f30c1d886d12a2b241277e116c6067c2bd1e1f4
TristynKahrs <79658190+TristynKahrs@users.noreply.github.com>
2023-03-05 13:36:04 -0700

    Email formatting

--- backend/models/Emailer.py
+++ backend/models/Emailer.py
@@ -4,6 +4,7 @@ from models.Environ import Environ
 from models.Database import initialize_db
 from models.Receipt import Receipt
 from models.User import User
+from models.Book import Book
 
 
 conf = ConnectionConfig(
@@ -20,22 +21,43 @@ conf = ConnectionConfig(
 
 fastmail = FastMail(conf)
 
-initialize_db([Receipt, User])
+initialize_db([Receipt, User, Book])
 
 def send_email(reciept_id: str):
     asyncio.get_event_loop().run_until_complete(async_send_email(reciept_id))
 
+async def format_email(reciept: Receipt, user: User):
+    email_body = f'''<h3>{user.name} - {user.email}</h3><div><table><thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Author</th>
+                    <th>Price</th>
+                </tr>
+            </thead><tbody>'''
+    
+    for book_id, price in reciept.purchases.items():
+        book: Book = await Book.get(book_id)
+        email_body += f'''<tr>
+                <td>{book.id}</td>
+                <td>{book.title}</td>
+                <td>{book.genre}</td>
+                <td>{book.author}</td>
+                <td>{"${:.2f}".format(price)}</td>
+            </tr>'''
+        
+    email_body += f'''</tbody></table><h3>${reciept.total_price} issued to StripeID:[{reciept.stripe_payment_id}] at {reciept.datetime}</h3></div>'''
+    return str(email_body)
+
 async def async_send_email(reciept_id: str):
     reciept: Receipt = await Receipt.get(reciept_id)
     user: User = await User.get(reciept.user_id)
-    email = user.email
-    
-    email_body = str(reciept.purchases)
-    # TODO: format email body html
+    email_body = await format_email(reciept, user)
 
     message = MessageSchema(
-        subject=f"Reciept to {email}",
-        recipients=[email],
+        subject=f"Book Order for {user.name}",
+        recipients=[user.email],
         body=email_body,
         subtype=MessageType.html
     )
